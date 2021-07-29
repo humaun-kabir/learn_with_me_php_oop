2954cc6d9110dc4eff772728dd3b6157a19584a4
humaun-kabir <asmhumaunkabir@gmail.com>
2021-07-28 22:32:39 -0700

    constant

---
+++ constant.php
@@ -0,0 +1,39 @@
+<?php
+    class UserData{
+        public $user;
+        public $userId;
+
+        //constant declare 
+        const NAME = "Humaun Kabir";
+
+        //rules constant declare
+        // class_name::const_name;
+
+        public function __construct($userName,$userId){
+            $this->user = $userName;
+            $this->userId = $userId;
+
+            echo "Username is {$this->user} and userid is {$this->userId}";
+        }
+
+        public function display(){
+            echo "Full name is".UserData::NAME;
+        }
+
+        public function __destruct()
+        {
+            unset($this->user);
+            unset($this->userId);
+            
+        }
+        
+    }
+
+    $user = "humaun";
+    $id = "25";
+    $ur = new UserData($user,$id);
+    echo "<br>";
+    $ur->display();
+
+
+?>
