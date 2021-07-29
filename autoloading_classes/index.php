ccf43ad6ec429807359795df2a4f8becdfa25b8c
humaun-kabir <asmhumaunkabir@gmail.com>
2021-07-29 07:38:13 -0700

    autoload classes

---
+++ autoloading_classes/index.php
@@ -0,0 +1,14 @@
+<?php
+spl_autoload_register(function($class_name){
+    include "classes/".$class_name.".php";
+});
+// another way to auload class
+/*
+function __autoload($class_name){
+    include "classes/".$class_name.".php";
+}
+*/
+$ruby = new Ruby();
+$java = new Java();
+$php = new Php();
+?>
